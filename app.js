a33439555110ca77d038a8d3c41d43cb09e59211
Dailey Kaze <dkaze@Daileys-MacBook-Air.local>
2021-11-04 14:16:10 -0400

    added smooth scrolling

--- app.js
+++ app.js
@@ -1,42 +1,85 @@
 const handleTyping = async () => {
-    const name = "./Dailey_Kaze"
-    const nameLetters = name.split("")
+    const name = "./Dailey_Kaze";
+    const nameLetters = name.split("");
 
-    const nameElem = document.getElementById("name")
+    const nameElem = document.getElementById("name");
 
     let i = 0;
 
     while (i < name.split("").length) {
         await waitForMs(400);
-        nameElem.append(nameLetters[i])
-        i++
-    }
+        nameElem.append(nameLetters[i]);
+        i++;
+    };
 
     document.getElementsByClassName("cursor")[0].remove();
 
     setTimeout(async () => {
 
 
-        const role = "Full Stack Developer.exe"
-        const roleLetters = role.split("")
+        const role = "Full Stack Developer.exe";
+        const roleLetters = role.split("");
     
-        const elem = document.getElementById("role")
+        const elem = document.getElementById("role");
 
         let i = 0;
     
         while (i < roleLetters.length) {
             await waitForMs(200);
-            elem.append(roleLetters[i])
-            i++
-        }
+            elem.append(roleLetters[i]);
+            i++;
+        };
     
-        document.getElementsByClassName("cursor")[0].remove()
-    }, 2500)    
-}
+        document.getElementsByClassName("cursor")[0].remove();
+    }, 2500);    
+};
 
 const waitForMs = (ms)  =>{
-    return new Promise(resolve => setTimeout(resolve, ms))
+    return new Promise(resolve => setTimeout(resolve, ms));
+};
+
+window.addEventListener('load', handleTyping);
+
+const showMenu = () => {
+    document.getElementById("menu-dropdown").classList.toggle("show");
+};
+
+// handle onClose
+window.onclick = event => {
+    if (!event.target.matches('.fa-bars')) {
+        const dropdowns = document.getElementsByClassName("dropdown-content");
+        
+        for (let i = 0; i < dropdowns.length; i++) {
+          let openDropdown = dropdowns[i];
+          if (openDropdown.classList.contains('show')) {
+            openDropdown.classList.remove('show');
+          }
+        }
+    }
 }
 
-window.addEventListener('load', handleTyping)
+// handle showing scrollToTop btn
+window.addEventListener("scroll", () => {
+    if (window.innerHeight + window.scrollY >= document.body.clientHeight)
+    document.getElementsByClassName("top-icon")[0].style.display = "initial";
+   else if (window.innerHeight > window.scrollY) document.getElementsByClassName("top-icon")[0].style.display = "none";
+})
 
+// handle scroll to top of page
+const scrollToTop = document.getElementsByClassName("top-icon")[0]
+
+scrollToTop.onclick = () => {
+   window.scrollTo({
+       top: 0,
+       behavior: "smooth"
+   })
+}
+
+// handle "q"
+window.addEventListener("keydown", (e) => {   
+    if (e.key === "q"){
+        if (window.innerHeight > window.scrollY) document.getElementById("projects").scrollIntoView({ behavior: "smooth"});
+        else document.getElementById("about").scrollIntoView({ behavior: "smooth"});
+        
+    }
+})
